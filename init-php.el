ccecd2d9871ec4463a14a70211c805e6988ffdca
Rifqi Fachriza <fachriza@outlook.com>
2013-01-20 19:03:56 +0700

    add phplint

--- init-php.el
+++ init-php.el
@@ -19,6 +19,19 @@
   (set (make-local-variable 'compile-command) (format "php_lint %s" (buffer-file-name))))
 (add-hook 'php-mode-hook 'my-php-hook-function)
 
+
+(defun flymake-php-init ()
+  "Use php and phpcs to check the syntax and code compliance of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+     (local (file-relative-name temp (file-name-directory buffer-file-name))))
+    (list "php_lint" (list local))))
+
+
+;;This is the error format for : php -f somefile.php -l 
+(add-to-list 'flymake-err-line-patterns
+  '("\(Parse\|Fatal\) error: +\(.?\) in \(.?\) on line \([0-9]+\)$" 3 4 nil 2))
+
+
 (add-to-list 'flymake-allowed-file-name-masks '("\.php$" flymake-php-init))
 (add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
 
